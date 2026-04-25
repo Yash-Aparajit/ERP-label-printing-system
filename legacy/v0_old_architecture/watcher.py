5a2f28455713b88f6f55364454b659708438e84e
Yash Aparajit <yashaparajit995@gmail.com>
2026-04-25 16:35:18 +0530

    Refactor watcher.py and clean up app.py
    Removed unused imports and database setup code from app.py. Simplified the watcher.py file.

---
+++ legacy/v0_old_architecture/watcher.py
@@ -0,0 +1,36 @@
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from generator import generate_label
+
+
+class TXTHandler(FileSystemEventHandler):
+
+    def __init__(self, input_folder, output_folder, log_callback):
+        self.input_folder = input_folder
+        self.output_folder = output_folder
+        self.log_callback = log_callback
+
+    def on_created(self, event):
+
+        if event.src_path.endswith(".txt"):
+
+            with open(event.src_path,"r") as f:
+                line = f.read().strip()
+
+            parts = line.split(",")
+
+            pdf = generate_label(parts, self.output_folder)
+
+            self.log_callback(parts[1], parts[0], pdf)
+
+
+def start_watcher(input_folder, output_folder, log_callback):
+
+    event_handler = TXTHandler(input_folder, output_folder, log_callback)
+
+    observer = Observer()
+    observer.schedule(event_handler, input_folder, recursive=False)
+    observer.start()
+
+    return observer
