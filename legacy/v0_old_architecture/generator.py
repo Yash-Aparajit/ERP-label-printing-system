dde9194f8f9b02af9b74395c79d7c8144da45538
Yash Aparajit <yashaparajit995@gmail.com>
2026-04-27 10:53:03 +0530

    Refactor label drawing function and update layout

--- legacy/v0_old_architecture/generator.py
+++ legacy/v0_old_architecture/generator.py
@@ -5,18 +5,18 @@ import barcode
 from barcode.writer import ImageWriter
 
 
-def draw_single_label(c, plant, ul, edi, inv, qty, created, time, y_offset):
+def draw_label(c, plant, ul, edi, inv, qty, created, time):
 
     c.setFont("Courier-Bold",9)
 
-    c.drawString(3*mm, y_offset + 33*mm, f"U/L COUNTER : {ul}")
-    c.drawRightString(72*mm, y_offset + 33*mm, plant)
+    c.drawString(3*mm,34*mm,f"U/L COUNTER : {ul}")
+    c.drawRightString(72*mm,34*mm,plant)
 
-    c.drawString(3*mm, y_offset + 29*mm, f"EDI NUMBER : {edi}")
-    c.drawString(3*mm, y_offset + 25*mm, f"INV/DATE : {inv}")
-    c.drawString(3*mm, y_offset + 21*mm, f"EDI/REC. QTY : {qty}")
-    c.drawString(3*mm, y_offset + 17*mm, f"CREATED BY : {created}")
-    c.drawString(3*mm, y_offset + 13*mm, f"C. DATE/TIME : {time}")
+    c.drawString(3*mm,30*mm,f"EDI NUMBER : {edi}")
+    c.drawString(3*mm,26*mm,f"INV/DATE : {inv}")
+    c.drawString(3*mm,22*mm,f"EDI/REC. QTY : {qty}")
+    c.drawString(3*mm,18*mm,f"CREATED BY : {created}")
+    c.drawString(3*mm,14*mm,f"C. DATE/TIME : {time}")
 
     options = {
         "write_text": False,
@@ -28,7 +28,7 @@ def draw_single_label(c, plant, ul, edi, inv, qty, created, time, y_offset):
     code = barcode.get('code128', ul, writer=ImageWriter())
     barcode_file = code.save("temp_barcode", options)
 
-    c.drawImage(barcode_file, 5*mm, y_offset + 2*mm, width=60*mm, height=9*mm)
+    c.drawImage(barcode_file,5*mm,2*mm,width=60*mm,height=9*mm)
 
     os.remove(barcode_file)
 
@@ -39,12 +39,20 @@ def generate_label(data, output_folder):
 
     pdf_path = os.path.join(output_folder, f"{ul}.pdf")
 
-    # 3 stickers per page
-    c = canvas.Canvas(pdf_path, pagesize=(75*mm, 114*mm))
+    # correct sticker size
+    c = canvas.Canvas(pdf_path, pagesize=(75*mm,38*mm))
 
-    draw_single_label(c, plant, ul, edi, inv, qty, created, time, 76*mm)
-    draw_single_label(c, plant, ul, edi, inv, qty, created, time, 38*mm)
-    draw_single_label(c, plant, ul, edi, inv, qty, created, time, 0)
+    # page 1
+    draw_label(c, plant, ul, edi, inv, qty, created, time)
+    c.showPage()
+
+    # page 2
+    draw_label(c, plant, ul, edi, inv, qty, created, time)
+    c.showPage()
+
+    # page 3
+    draw_label(c, plant, ul, edi, inv, qty, created, time)
 
     c.save()
+
     return pdf_path
