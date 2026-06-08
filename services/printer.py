425c8a66d49f73e66a4f9c40b9bb9d2cbb27ac53
Yash Aparajit <yashaparajit995@gmail.com>
2026-06-08 14:36:25 +0530

    Add delay before printing PDF to ensure file readiness
    Added a delay before printing to ensure file write completion.

--- services/printer.py
+++ services/printer.py
@@ -1,8 +1,19 @@
 import os
+import time
+
 
 def print_pdf(path):
 
     try:
+
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"PDF not found: {path}")
+
+        # small delay to ensure file write completion
+        time.sleep(0.2)
+
         os.startfile(path, "print")
+
     except Exception as e:
+
         print("Printer error:", e)
